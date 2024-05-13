e6262b51e6d92d99a5954ee9d1cd84bca48d9f97
daiki_beppu (別府 大貴) <beppu.engineer@gmail.com>
2024-05-13 22:14:27 +0900

    layout:(about)
    フッターとコンテンツの間にpaddingを追加

--- app/about/components/features.tsx
+++ app/about/components/features.tsx
@@ -4,7 +4,7 @@ import Card from "./card";
 export default function Features() {
   return (
     <Section title={"サービスの特徴"} subTitle={"すごいサービスがたくさん！"}>
-      <div className="grid lg:grid-cols-3 gap-4 items-center">
+      <div className="pb-20 grid lg:grid-cols-3 gap-4 items-center">
         <Card />
         <Card />
         <Card />
