694c5d3e5ddddddae4b5d4fbab328ab66604c955
daiki_beppu (別府 大貴) <beppu.engineer@gmail.com>
2024-05-22 20:27:01 +0900

    add(home)
    記事一覧の追加

--- lib/newt.ts
+++ lib/newt.ts
@@ -19,6 +19,7 @@ export const getArticles = cache(async () => {
         "title",
         "slug",
         "body",
+        "coverImage",
         "_sys.createdAt",
         "_sys.updatedAt",
       ],
--- types/article.ts
+++ types/article.ts
@@ -1,8 +1,12 @@
 export interface Article {
   _id: string;
+  src: string;
   title: string;
   slug: string;
   body: string;
+  coverImage: {
+    src: string;
+  };
   _sys: {
     createdAt: string;
     updatedAt: string;
